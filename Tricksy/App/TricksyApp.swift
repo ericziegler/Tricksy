0fef610e1b8dc0453b0dd57b00d6b5f9ca2118a5
Eric Ziegler <eric@zigamajig.com>
2022-10-30 15:38:20 -0400

    EZ: Initial commit.

---
+++ Tricksy/App/TricksyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TricksyApp.swift
+//  Tricksy
+//
+//  Created by Eric Ziegler on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TricksyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
